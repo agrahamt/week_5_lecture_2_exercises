874ed0717670aaee5cee1d65fb0de5296181b73a
Andrew Taylor <agrahamt@gmail.com>
2017-03-25 12:18:22 -0500

    commit exercise02.js

---
+++ exercise02.js
@@ -0,0 +1,8 @@
+// Loop from 0 to 99 and output the numbers that are multiples of 3.
+// Do not use conditional statements. IE: 0, 3, 6, 9, 12, etc...
+
+for (var i = 0; i <= 99; i += 3) {
+  if (i !== 0) {
+    console.log(i)
+  }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+**TIY week_5_lecture_2 homework**
